c873bc9da651c72d749363333dd0b8ddb1916fe6
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-08-12 08:57:15 +0600

    project init

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,13 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', '\App\Http\Controllers\Api\AuthController@register');
+Route::post('login', '\App\Http\Controllers\Api\AuthController@login');
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('/logout', '\App\Http\Controllers\Api\AuthController@logout');
+});
 
 Route::group([ 'namespace'=> '\App\Http\Controllers\Api\Admin', 'prefix' => 'admin',  'as'=>'admin.', 'middleware' => ['auth:api']], function () { 
   Route::get('/products/search/{title}', 'ProductController@search')->name('products.search'); 
