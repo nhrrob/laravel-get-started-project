ac67f5d41941aeeaaa5306531b784f08d30b932a
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-11-02 23:28:20 +0600

    AuthController removed without versioning

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,21 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::post('register', '\App\Http\Controllers\Api\AuthController@register');
-Route::post('login', '\App\Http\Controllers\Api\AuthController@login');
+// Route::post('register', '\App\Http\Controllers\Api\AuthController@register');
+// Route::post('login', '\App\Http\Controllers\Api\AuthController@login');
+
+// Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::post('/logout', '\App\Http\Controllers\Api\AuthController@logout');
+// });
+
+Route::post('v1/register', '\App\Http\Controllers\Api\V1\AuthController@register');
+Route::post('v1/login', '\App\Http\Controllers\Api\V1\AuthController@login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/logout', '\App\Http\Controllers\Api\AuthController@logout');
+    Route::post('v1/logout', '\App\Http\Controllers\Api\V1\AuthController@logout');
 });
 
-Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1\Admin', 'prefix' => 'v1',  'as'=>'api.', 'middleware' => ['auth:sanctum']], function () { 
-  Route::get('/products/search/{title}', 'ProductController@search')->name('products.search'); 
-  Route::apiResource('products', 'ProductController'); 
-});
+// Route::group([ 'namespace'=> '\App\Http\Controllers\Api\V1\Admin', 'prefix' => 'v1',  'as'=>'api.', 'middleware' => ['auth:sanctum']], function () { 
+//   Route::get('/products/search/{title}', 'ProductController@search')->name('products.search'); 
+//   Route::apiResource('products', 'ProductController'); 
+// });
