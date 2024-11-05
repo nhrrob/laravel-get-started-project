1704e30feb46a2269c16b65bc3c38cb90314c6d1
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-11-05 11:02:18 +0600

    Stubs

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,40 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Product') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product</title>
-</head>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <!-- Page Content: Starts -->
+                    <div class="container">
+                        @if (session('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('message') }}
+                        </div>
+                        @endif
 
-<body>
-    <div class="container">
-        <h4>Create Product</h4>
-        <hr>
-        
-        @if (session('message'))
-            <div class="alert alert-success" role="alert">
-                {{ session('message') }}
-            </div>
-        @endif
-        
-        <form method="POST" action='{{ route("products.store") }}' enctype="multipart/form-data">
-            @csrf
-            
-            <div class="form-group">
-                <input class="form-control" type="text" name="title" placeholder="Title">
-                @error('title')
-                <label class="text-danger">{{ $message }}</label>
-                @enderror
-            </div>
+                        <form method="POST" action='{{ route("products.store") }}' enctype="multipart/form-data">
+                            @csrf
 
-            <div class="form-group">
-                <a class="btn btn-danger mr-1" href='{{ route("products.index") }}' type="submit">Cancel</a>
-                <button class="btn btn-success" type="submit">Save</button>
+                            <div class="form-group">
+                                <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" type="text" name="title" placeholder="Title">
+                                @error('title')
+                                <label class="text-danger">{{ $message }}</label>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <a class="inline-flex items-center my-2 px-4 py-2 bg-gray-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href='{{ route("products.index") }}' type="submit">Cancel</a>
+                                <button class="inline-flex items-center my-2 px-4 py-2 bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" type="submit">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- Page Content: Ends  -->
+                </div>
+
+
             </div>
-        </form>
+        </div>
     </div>
-</body>
-
-</html>
+</x-app-layout>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,69 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <!-- {{ __('Product') }} -->
+            {{ __('Product') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product</title>
-</head>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <!-- Page Content: Starts  -->
+                    @if (session('message'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('message') }}
+                    </div>
+                    @endif
 
-<body>
+                    <p><a class="inline-flex items-center my-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href='{{ route("products.create") }}'><i class="fa fa-plus"></i> Create Product</a></p>
 
-    @if (session('message'))
-        <div class="alert alert-success" role="alert">
-            {{ session('message') }}
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Title
+                                </th>
+                                <th>
+                                    Created
+                                </th>
+                                <th width="5%">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($products as $product)
+                            <tr>
+                                <td>
+                                    {{ $product->title ?? 'N/A' }}
+                                </td>
+
+                                <td>
+                                    {{ optional($product->created_at)->diffForHumans() }}
+                                </td>
+
+                                <td>
+                                    <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href='{{ route("products.edit", $product->id) }}'><i class="fa fa-pencil"></i> Edit</a>
+
+                                    <form method="POST" action="{{ route('products.destroy', $product->id) }}">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <div class="form-group">
+                                            <i class="fa fa-times"></i>
+                                            <input type="submit" class="inline-flex items-center my-2 px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" value="Delete">
+                                        </div>
+                                    </form>
+
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="3" align="center">No records found!</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                    <!-- Page Content: Ends  -->
+                </div>
+
+
+            </div>
         </div>
-    @endif
-    
-    <p><a class="btn btn-success" href='{{ route("products.create") }}'><i class="fa fa-plus"></i> Create Product</a></p>
-    
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>
-                    Title
-                </th>
-                <th>
-                    Created
-                </th>
-                <th width="5%">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($products as $product)
-            <tr>
-                <td>
-                    {{ $product->title ?? 'N/A' }}
-                </td>
-
-                <td>
-                    {{ optional($product->created_at)->diffForHumans() }}
-                </td>
-
-                <td>
-                    <a class="btn btn-success d-block mb-2" href='{{ route("products.edit", $product->id) }}'><i class="fa fa-pencil"></i> Edit</a>
-                    
-                    <form method="POST" action="{{ route('products.destroy', $product->id) }}">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-
-                        <div class="form-group">
-                            <i class="fa fa-times"></i>
-                            <input type="submit" class="btn btn-danger d-block" value="Delete">
-                        </div>
-                    </form>
-                    
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="3" align="center">No records found!</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-
-</body>
-
-</html>
+    </div>
+</x-app-layout>
